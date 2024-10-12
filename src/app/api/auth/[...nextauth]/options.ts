f23c27cab58fdace4053d288376e72083234092b
tkp315 <114278931+tkp315@users.noreply.github.com>
2024-10-12 12:28:54 +0530

    next_auth

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,21 @@
+// pages/api/auth/[...nextauth].ts
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+
+  callbacks: {
+    async session({ session, token }) {
+      // You can add token information to the session object here if needed
+      return {session,token};
+    },
+  },
+};
+
+export default NextAuth(authOptions);
---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { useEffect } from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+
+export default function SignInPage() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  // Redirect if user is logged in
+  useEffect(() => {
+    if (status === "authenticated") {
+      router.push("/");
+    }
+  }, [status, router]);
+  useEffect(() => {
+    if (status !== "authenticated") {
+      router.push("/sign-in");
+    }
+  }, [status, router]);
+
+  return (
+    <div className=" flex justify-center items-center min-h-screen">
+      {status === "loading" ? (
+        <p>Loading...</p>
+      ) : !session ? (
+        <Button onClick={() => signIn("github")}>
+          Sign in with GitHub
+        </Button>
+      ) : (
+        <>
+          <p>Welcome, {session.user?.name}</p>
+        </>
+      )}
+    </div>
+  );
+}
