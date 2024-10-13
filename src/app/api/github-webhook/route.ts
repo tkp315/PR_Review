070b0f317e8121af44e265cf454afebd7f8594e5
tkp315 <114278931+tkp315@users.noreply.github.com>
2024-10-13 19:29:22 +0530

    written webhook code

---
+++ src/app/api/github-webhook/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function POST(req:NextRequest){
+    try {
+        const payload = await req.json();
+        console.log(payload);
+        const eventType = req.headers.get('x-github-event')
+        if(eventType==='pull_request'){
+            const prData = payload.pull_request;
+            console.log("Pull request Data",prData);
+            const prTitle = prData.title;
+            const prAuthor = prData.user.login;
+            const prState = prData.prData.state;
+            console.log(`Title:${prTitle},Author:${prAuthor},State:${prState}`)
+
+            return NextResponse.json({message:"pull request prcessed Successfully",prData})
+        }
+        return NextResponse.json({ message: 'Not a pull request event' }, { status: 400 });
+    } catch (error) {
+        console.error('Error handling GitHub webhook:', error);
+        return NextResponse.json({ message: 'Error processing webhook', error: error.message }, { status: 500 });
+    }
+
+}
